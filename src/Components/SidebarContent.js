3f53bb81834df8b4cb6460706b7b67d6c5c00d64
Evie-Mututa <evemututa29@gmail.com>
2023-11-12 21:54:17 +0300

    ReviewSection Added

--- src/Components/SidebarContent.js
+++ src/Components/SidebarContent.js
@@ -1,11 +1,44 @@
 // SidebarContent.js
-import React from "react";
+import React, { useState } from "react";
 import NavLinks from "./NavLinks";
 import CartProducts from "./CartProducts";
 import { useCart } from "./CartContext"; // Import useCart from the CartContext
 
 function SidebarContent() {
   const { cartData, removeFromCart, updateQuantity } = useCart(); // Use the useCart hook
+  const [reviews, setReviews] = useState([]);
+  const [newReview, setNewReview] = useState({ name: '', comment: '', rating: 0, profilePic: '' });
+  const [replyText, setReplyText] = useState('');
+  const [replyingTo, setReplyingTo] = useState(null);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewReview((prevReview) => ({ ...prevReview, [name]: value }));
+  };
+
+  const handleRatingChange = (newRating) => {
+    setNewReview((prevReview) => ({ ...prevReview, rating: newRating }));
+  };
+
+  const handleAddReview = () => {
+    setReviews([...reviews, { ...newReview, replies: [] }]);
+    setNewReview({ name: '', comment: '', rating: 0, profilePic: '' });
+  };
+
+  const handleReply = (index) => {
+    setReplyingTo(index);
+  };
+
+  const handleAddReply = (reviewIndex) => {
+    const updatedReviews = [...reviews];
+    updatedReviews[reviewIndex].replies = [
+      ...(updatedReviews[reviewIndex].replies || []),
+      { text: replyText, author: 'User123' }, // Assuming a fixed author for simplicity
+    ];
+    setReviews(updatedReviews);
+    setReplyText('');
+    setReplyingTo(null);
+  };
 
   return (
     <div className="sidebar-content">
@@ -43,13 +76,102 @@ function SidebarContent() {
       <div className="quick-links">
         <h3>Reviews</h3>
         <ul>
-          <li>
-            <a href="#contact">Contact Us</a>
-          </li>
-          <li>
-            <a href="#reviews">Customer Reviews</a>
-          </li>
-          {/* Add more quick links as needed */}
+            <div>
+              {/* Review Section */}
+              <div className="review-section">
+                <h4>Add Your Review</h4>
+                {/* An input for the profile picture */}
+                <input
+                  type="text"
+                  placeholder="Profile Picture URL"
+                  name="profilePic"
+                  value={newReview.profilePic}
+                  onChange={handleInputChange}
+                />
+                <input
+                  type="text"
+                  placeholder="Your Name"
+                  name="name"
+                  value={newReview.name}
+                  onChange={handleInputChange}
+                />
+                <textarea
+                  placeholder="Your Comment"
+                  name="comment"
+                  value={newReview.comment}
+                  onChange={handleInputChange}
+                />
+                <div>
+                  <span>Rating:</span>
+                  {/* Display stars horizontally */}
+                  <div className="star-rating">
+                    {Array.from({ length: 5 }).map((_, index) => (
+                      <span
+                        key={index}
+                        className={`star ${index < newReview.rating ? 'filled' : ''}`}
+                        onClick={() => handleRatingChange(index + 1)}
+                      >
+                        &#9733;
+                      </span>
+                    ))}
+                  </div>
+                </div>
+                <button onClick={handleAddReview}>Add Review</button>
+              </div>
+              {/* Display Reviews */}
+              <div className="reviews-list">
+                {reviews.map((review, reviewIndex) => (
+                  <div key={reviewIndex} className="review-item">
+                    <div>
+                      <img
+                        src={review.profilePic}
+                        alt={`Profile Pic of ${review.name}`}
+                        className="profile-pic"
+                      />
+                    </div>
+                    <div>{review.name}</div>
+                    <div>{review.comment}</div>
+                    <div>
+                      {/* Display stars horizontally */}
+                      <div className="star-rating">
+                        {Array.from({ length: 5 }).map((_, i) => (
+                          <span
+                            key={i}
+                            className={`star ${i < review.rating ? 'filled' : ''}`}
+                          >
+                            &#9733;
+                          </span>
+                        ))}
+                      </div>
+                    </div>
+                    {/* Reply Section */}
+                    <div className="reply-section">
+                      <button onClick={() => handleReply(reviewIndex)}>
+                        {replyingTo === reviewIndex ? 'Cancel Reply' : 'Reply'}
+                      </button>
+                      {replyingTo === reviewIndex && (
+                        <>
+                          <textarea
+                            placeholder="Your Reply"
+                            value={replyText}
+                            onChange={(e) => setReplyText(e.target.value)}
+                          />
+                          <button onClick={() => handleAddReply(reviewIndex)}>Add Reply</button>
+                        </>
+                      )}
+                    </div>
+                    {/* Display Replies */}
+                    {review.replies &&
+                      review.replies.map((reply, replyIndex) => (
+                        <div key={replyIndex} className="reply-item">
+                          <div>{reply.author}</div>
+                          <div>{reply.text}</div>
+                        </div>
+                      ))}
+                  </div>
+                ))}
+              </div>
+            </div>
         </ul>
       </div>
 
@@ -90,3 +212,4 @@ function SidebarContent() {
 }
 
 export default SidebarContent;
+
